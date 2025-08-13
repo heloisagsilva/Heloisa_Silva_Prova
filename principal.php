c0f3fb2551fcbeef9525532cb00de40c06f30b5d
heloisagsilva <helogsilva2909@gmail.com>
2025-08-13 14:24:31 -0300

    espaço
    apenas adicionei espaçamento

--- principal.php
+++ principal.php
@@ -20,8 +20,11 @@
     $permissoes = [
         //ADMIN
         1 => ["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+              
               "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+              
               "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+              
               "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]],
 
         //SECRETÁRIA
