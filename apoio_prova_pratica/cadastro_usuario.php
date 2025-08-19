5c6bbfff2ff9d093b628977ac40c241f0d9f5ed1
heloisagsilva <helogsilva2909@gmail.com>
2025-08-19 14:08:18 -0300

    foi

--- apoio_prova_pratica/cadastro_usuario.php
+++ apoio_prova_pratica/cadastro_usuario.php
@@ -1,34 +1,33 @@
 <?php
 session_start();
-require_once('conexao.php');
-require_once('permissoes.php');
+require_once 'conexao.php';
+require_once ('permissoes.php');
 
-// Verifica se o usuário tem permissão supondo que o perfil 1 sejá o admin
-if($_SESSION['perfil'] != 1){
+//VERIFICA SE O USUARIO TEM PERMISSAO SUPONDE QUE O PERFIL 1 SEJA O ADMINISTRADOR
+if($_SESSION['perfil']!=1){
     echo "Acesso negado!";
 }
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $nome = $_POST['nome'];  
-    $email = $_POST['email'];  
-    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);  
-    $id_perfil = $_POST['id_perfil'];  
-    
-    $sql = "INSERT INTO usuario(nome,email,senha,id_perfil)
-            VALUES (:nome,:email,:senha,:id_perfil)";
-    $stmt = $pdo -> prepare($sql);
-    $stmt -> bindParam(":nome",$nome);
-    $stmt -> bindParam(":email",$email);
-    $stmt -> bindParam(":senha",$senha);
-    $stmt -> bindParam(":id_perfil",$id_perfil);
-    
-    if($stmt -> execute()){
-        echo "<script>alert('Usuário cadastrado com sucesso!');</script>";
-    } else{
-        echo "<script>alert('Erro ao cadastrar usuário');</script>";
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+    $nome= $_POST['nome'];
+    $email= $_POST['email'];
+    $senha= password_hash($_POST['nome'], PASSWORD_DEFAULT);
+    $id_perfil= $_POST['id_perfil'];
+
+    $sql= "INSERT INTO usuario (nome, email, senha, id_perfil) VALUES (:nome, :email, :senha, :id_perfil)";
+    $stmt= $pdo-> prepare($sql);
+    $stmt-> bindParam(':nome', $nome);
+    $stmt-> bindParam(':email', $email);
+    $stmt-> bindParam(':senha', $senha);
+    $stmt-> bindParam(':id_perfil', $id_perfil);
+
+    if($stmt-> execute()){
+        echo "<script> ('Usuario cadastrado com sucesso!!');</script";
+    }
+    else{
+        echo "<script> alert('Erro ao encontrar usuario');</script>";
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -36,35 +35,54 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css"/>
-    <script src="validacao.js"></script>
-    <title>Cadastrar usuário</title>
+    <title>Cadastro Usuario</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
 <body>
-     
-     <h2>Cadastrar usuário</h2>
 
-     <form action="cadastro_usuario.php" method="POST">
-        <label for="nome">Nome: </label>
-        <input type="text" id="nome" name="nome" required/>
+<nav>
+        <ul class="menu">
+            <?php foreach($opcoes_menu as $categoria => $arquivos) { ?>
+                <li class="dropdown">
+                    <a href="#"><?= $categoria ?></a>
 
-        <label for="email">E-mail: </label>
-        <input type="email" id="email" name="email" required/>
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo) { ?>
+                            <li>   
+                                <a href="<?= $arquivo ?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php"))) ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </li>
+            <?php } ?>
+        </ul>
+    </nav>
+<br>
+    <a href="principal.php" class="btn btn-outline-primary">Voltar</a>
 
-        <label for="senha">Senha: </label>
-        <input type="password" id="senha" name="senha" required/>
+    <h2 align="center"> Cadastrar Usuario </h2>
+    <form action="cadastro_usuario.php" method="POST">
+        <label for="nome"> Nome: </label>
+        <input type="text" id="nome" name="nome" required>
 
-        <label for="id_perfil">Perfil: </label>
+        <label for="email"> Email: </label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="senha"> Senha: </label>
+        <input type="password" id="senha" name="senha" required>
+
+        <label for="id_perfil"> Perfil: </label>
         <select id="id_perfil" name="id_perfil">
-            <option value="1">Administrador</option>
-            <option value="2">Secretária</option>
-            <option value="3">Almoxarife</option>
-            <option value="4">Cliente</option>
+            <option value="1"> Administrador </option>
+            <option value="2"> Secretária </option>
+            <option value="3"> Almoxarife </option>
+            <option value="4"> Cliente </option>
         </select>
-
-        <button type="submit">Salvar</button>
-        <button type="reset">Cancelar</button>
-     </form>
-     <a href="principal.php">Voltar</a>
+<br>
+    <button type="submit" class="btn btn-outline-success"> Salvar </button> <br>
+    <button type="reset" class="btn btn-outline-danger"> Cancelar </button>
+</form>
+<a href="principal.php" class="btn btn-outline-primary">Voltar</a>
 </body>
 </html>
