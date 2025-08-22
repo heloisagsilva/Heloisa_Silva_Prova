c023491ee988e1cfc07b0ae571a4e0f9d430ed73
heloisagsilva <helogsilva2909@gmail.com>
2025-08-22 16:46:15 -0300

    buscar usuario
    arquivo responsavel por buscar o usuario pelo nome ou id quando necessario

---
+++ buscar_usuario.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+require_once 'conexao.php';
+require_once ('permissoes.php');
+
+//VERIFCA SE O USARIO TEM PERMISSAO DE adm OU secretaria
+if($_SESSION['perfil'] !=1 && $_SESSION['perfil'] !=2){
+    echo "<script>alert('Acesso negado!');windown.location.href='principal.php';</script>";
+    exit();
+}
+$usuario= []; //INICIALIZA A VARIAVEL PARA EVITAR ERROS
+
+//SE FORMULARIO FOR ENVIADO, BUSCA USUARIO PELO O ID OU NOME
+if($_SERVER["REQUEST_METHOD"]== "POST" && !empty($_POST['busca'])){
+    $busca= trim($_POST['busca']);
+
+//VERIFICA SE A BUSCA É UM NUMERO OU UM NOME 
+if(is_numeric($busca)){
+    $sql= "SELECT * FROM usuario WHERE id_usuario = :busca ORDER BY nome ASC";
+    $stmt= $pdo-> prepare($sql);
+    $stmt-> bindParam(':busca', $busca, PDO::PARAM_INT);
+}else {
+    $sql= "SELECT * FROM usuario WHERE nome LIKE :busca_nome ORDER BY nome ASC";
+    $stmt= $pdo-> prepare($sql);
+    $stmt-> bindValue(':busca_nome', "$busca%", PDO::PARAM_STR);
+}
+}else {
+    $sql= "SELECT * FROM usuario ORDER BY nome ASC";
+    $stmt= $pdo-> prepare($sql);
+}
+
+$stmt-> execute();
+$usuarios= $stmt-> fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Usuario</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+
+<nav>
+        <ul class="menu">
+            <?php foreach($opcoes_menu as $categoria => $arquivos) { ?>
+                <li class="dropdown">
+                    <a href="#"><?= $categoria ?></a>
+
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo) { ?>
+                            <li>   
+                                <a href="<?= $arquivo ?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php"))) ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </li>
+            <?php } ?>
+        </ul>
+    </nav>
+<br>
+    <a href="principal.php" class="btn btn-outline-primary">Voltar</a>
+
+    <h2 align="center"> Lista de Usuarios </h2>
+    <form action="buscar_usuario.php" method="POST">
+        <label for="busca"> Digite o ID ou NOME(opcional): </label>
+        <input type="text" id="busca" name="busca">
+        <button type="submit"> Pesquisar</button>
+    </form>
+
+    <?php if(!empty($usuarios)):?>
+        <table class="table" border="1" align="center">
+            <tr>
+                <th> ID </th>
+                <th> NOME </th>
+                <th> EMAIL </th>
+                <th> PERFIL </th>
+                <th> AÇÕES </th>
+            </tr>
+            <?php foreach($usuarios as $usuario):?>
+            <tr>
+                <td> <?=htmlspecialchars($usuario['id_usuario'])?></td>
+                <td> <?=htmlspecialchars($usuario['nome'])?></td>
+                <td> <?=htmlspecialchars($usuario['email'])?></td>
+                <td> <?=htmlspecialchars($usuario['id_perfil'])?></td>
+                <td> 
+                    <a href="alterar_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>">Alterar</a>
+                    <a href="excluir_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>"onclick="return confirm
+                            ('Ter certeza que deseja excluir este usuario?')">Excluir</a>
+                </td>
+            </tr>
+            <?php endforeach;?>
+        </table>
+        <?php else:?>
+            <p> Nenhum usuario encontrado.</p>
+        <?php endif;?>
+        <br>
+</body>
+<br>
+<adress>
+    <center>
+        Heloisa Gonçalves da Silva/ Desenvolvimento de Sistemas
+    </center>
+</adress>
+</html>
